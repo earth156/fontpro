98708ad26cd0a9688a52519e88210d695af84c6e
earth156 <65011212091@msu.ac.th>
2024-03-17 09:51:29 +0700

    g

--- src/app/components/vote/vote.component.ts
+++ src/app/components/vote/vote.component.ts
@@ -33,7 +33,7 @@ export class VoteComponent implements OnInit, OnDestroy {
   constructor(private router: Router, private route: ActivatedRoute, private httpClient: HttpClient) {}
 
   async ngOnInit() {
-    const HOST: string = 'http://localhost:4000';
+    const HOST: string = 'https://backpro-qj8e.vercel.app';
     const url = `${HOST}/facemash/vote`;
     this.getCurrentDateTime(); // เรียกใช้งาน getCurrentDateTime() ใน ngOnInit()
     interval(1000)
@@ -64,7 +64,7 @@ export class VoteComponent implements OnInit, OnDestroy {
   }
 
   async vote(winnerPostId: number, loserPostId: number) {
-    const URL = 'http://localhost:4000/facemash/vote';
+    const URL = 'https://backpro-qj8e.vercel.app/facemash/vote';
     
     try {
         const response = await axios.post(URL, { winnerPostId, loserPostId });
